1064a8627b54ebcf918d815b75f309da86e031c0
fasyadita <fasyadita1@gmail.com>
2025-05-23 15:59:54 +0700

    update service landing page

--- resources/views/layouts/services.blade.php
+++ resources/views/layouts/services.blade.php
@@ -1,10 +1,9 @@
 
 <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-    <div class="row gy-5">
-
-      <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-        <div class="service-item">
+  <div class="row gy-5 justify-content-center">
+    <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+      <!-- Form Lulusan -->
+      <div class="service-item">
           <div class="img">
             <img src=" {{ asset('dewi/assets/img/lulusan2.jpeg')}} " class="img-fluid" alt="">
           </div>
@@ -19,10 +18,10 @@
             studi lanjut, dan pengalaman setelah lulus sebagai bagian dari tracer study.</p>
           </div>
         </div>
-      </div><!-- End Service Item -->
-
-      <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-        <div class="service-item">
+    </div>
+    <div class="col-md-4">
+      <!-- Form Instansi -->
+       <div class="service-item">
           <div class="img">
             <img src=" {{ asset('dewi/assets/img/boss2.jpeg') }}" class="img-fluid" alt="">
           </div>
@@ -36,8 +35,6 @@
             <p>Form ini diisi oleh pihak perusahaan atau instansi untuk memberikan penilaian dan masukan terhadap lulusan.</p>
           </div>
         </div>
-      </div><!-- End Service Item -->
-
     </div>
-
   </div>
+</div>
